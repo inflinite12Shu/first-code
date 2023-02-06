b9fe2ecaf3ee36a62a88816d4d8b980552f499a6
Yasir Arfat Mallah <60608647+inflinite12Shu@users.noreply.github.com>
2023-02-06 07:28:06 +0000

    Pending changes exported from your codespace

---
+++ anwers.py
@@ -0,0 +1,52 @@
+q1 = "Q no 1 : Who developed Python Programming Language?"
+q2 = "Q no 2 : Which type of programming does python support?"
+q3 = "Q no 3 : Is Python case sensitive when dealing with identifiers?"
+q4 = "Q no 4 : What is the correct extension of Python file?"
+q5 = "Q no 5 : Is Python code is compiled or interpreted?"
+q6 = "Q no 6 : Name a few blocks of code use to define in Python language."
+q7 = "Q no 7 : State a character used to give single line comments in Python."
+q8 = "Q no 8 : Mention function which can help us to find the version of python that "
+q9 = "Q no 9 : we are currently working on."
+q10 = "Q no 10 : What does PIP stand for Python?"
+q11 = "Q no 11 : Mention few built in function in python.."
+q12 = "Q no 12 : What is maximum possible length of an identifier in Python?"
+q13 = "Q no 13 : What are the benefits of using Python?"
+a1 = "Ans : Guido van Rossum"
+a2 = "Ans : Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming. "
+a3 = "Ans : Yes, Python is case sensitive when dealing with identifiers."
+a4 = "Ans : The correct extension for Python files is ".py"."
+a5 = "Ans : Python code is interpreted, not compiled."
+a6 = "Ans : blocks include functions, classes, and loops."
+a7 = "Ans : The character '#' is used to give single line comments in Python."
+a8 = "Ans : The 'sys' module provides a 'version' attribute that can be used to find the version of Python you are currently working on."
+a9 = "Ans : PIP stands for "Pip Installs Packages"."
+a10 = "Ans : ome built-in functions in Python include 'print()', 'len()', 'input()', and 'range()'."
+a11 = "Ans : The maximum possible length of an identifier in Python is not defined by the language specification, but it is limited by the amount of memory available to the interpreter."
+a12 = "Ans : The benefits of using Python include readability and ease of use, large and supportive community, vast array of libraries and modules, and versatility in application."
+a13 = "Ans : Some benefits of using Python include readability and ease of use, a large and supportive community, a vast array of libraries and modules, versatility in application, and a strong emphasis on simplicity and minimalism."
+print(q1)
+print(a1)
+print(q2)
+print(a2)
+print(q3)
+print(a3)
+print(q4)
+print(a4)
+print(q5)
+print(a5)
+print(q6)
+print(a6)
+print(q7)
+print(a7)
+print(q8)
+print(a8)
+print(q9)
+print(a9)
+print(q10)
+print(a10)
+print(q11)
+print(a11)
+print(q12)
+print(a12)
+print(q13)
+print(a13)
